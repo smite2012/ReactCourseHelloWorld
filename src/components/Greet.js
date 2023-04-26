eb1b1481955595a13e0eaf3b63b6b0326d45a248
wowanik <wowanik@syneforge.com>
2023-04-26 09:06:21 +0300

    fixed props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,6 +4,9 @@ import React from "react";
   return <h1>Hello User!</h1>
 }*/
 
-const Greet = () => <h1>Hello User!</h1>
+const Greet = (props) => {
+  console.log(props)
+  return <h1>Hello {props.name}! a.k.a {props.heroName}</h1>
+}
 
 export default Greet
