9ec3fb28f4cd845850c6faeb52ae5814674d9c49
wowanik <wowanik@syneforge.com>
2023-04-26 08:56:49 +0300

    component without JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Hello = () => {
-  return (
-    <div>
+  /*return (
+    <div className='myClass'>
       <h1>Hello user!</h1>
     </div>
+  )*/
+  return React.createElement(
+    'div',
+    {id: 'hello', className: 'myClass'},
+    React.createElement('h1', null, 'Hello user!')
   )
 }
-
 export default Hello
