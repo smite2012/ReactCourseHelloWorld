766aa885520697e9774194164ece903bee85aa4e
wowanik <wowanik@syneforge.com>
2023-05-15 09:22:43 +0300

    Destructuring props and state

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <Counter/>
+     {/* <Counter/>*/}
       {/*<Message/>*/}
-      {/*<Greet name="Bruce" heroName="Batman">
+      {/*{<Greet name="Bruce" heroName="Batman">
         <p>This is children props</p>
-      </Greet>
+      </Greet>}
       <Greet name="Clark" heroName="Superman">
         <button>Action</button>
-      </Greet>
+      </Greet>*/}
       <Greet name="Diana" heroName="Wonder Woman"/>
 
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/>
+      {<Welcome name="Bruce" heroName="Batman"/>
+      /*<Welcome name="Clark" heroName="Superman"/>
       <Welcome name="Diana" heroName="Wonder Woman"/>*/}
       {/*<Hello/>*/}
     </div>
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,15 +2,16 @@ import React from "react";
 /*function Greet(){
   return <h1>Hello User!</h1>
 }*/
-const Greet = (props) => {
+const Greet = props => {
+  const {name, heroName} = props
   console.log(props)
   return (
     <div>
       <h1>
-        Hello {props.name}! a.k.a {props.heroName}
+        Hello {name}! a.k.a {heroName}
       </h1>
-      {props.children}
     </div>
   )
+  console.log(props.children)
 }
 export default Greet
