c112f7a00d0785adc69f8b2f46ef4b30324d824e
wowanik <wowanik@syneforge.com>
2023-05-17 11:41:16 +0300

    17 - List Rendering - third example

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana']
-  const nameList = names.map(name => <h2>{name}</h2>)
-  return <div>{nameList}</div>
+  const persons = [
+    {
+      id: 1,
+      name: 'Bruce',
+      age: 30,
+      skill: 'React'
+    },
+    {
+      id: 2,
+      name: 'Clark',
+      age: 25,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Diana',
+      age: 28,
+      skill: 'Vue'
+    }
+  ]
+  const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
+  return <div>{personList}</div>
 }
 
 export default NameList
